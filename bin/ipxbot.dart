730c5b6c7b71e5af70251a3214704e318a3d30db
Bruno Boettcher <nokumado@gmail.com>
2021-11-28 17:55:29 +0100

    before sznchro

--- bin/ipxbot.dart
+++ bin/ipxbot.dart
@@ -1,21 +1,20 @@
 import 'package:args/args.dart';
 import 'package:ipxbot/ipxbot.dart' as ipxbot;
 import 'package:ipxbot/ipxbot.dart';
-import 'package:matrix_api_lite/matrix_api_lite.dart';
 
 void main(List<String> arguments)
 {
   var parser = ArgParser();
   parser.addOption('server', abbr: 's', defaultsTo: 'https://matrix.org');
   parser.addOption('user', abbr: 'u', defaultsTo: 'testbot');
-  parser.addOption('passwd', abbr: 'p', defaultsTo: 'nuthing');
+  parser.addOption('passwd', abbr: 'p', defaultsTo: 'nothing');
   parser.addOption('room', abbr: 'r', defaultsTo: '#testchan');
   parser.addOption('roomid', abbr: 'i');
   parser.addOption('invite', abbr: 'c');
   parser.addOption('msg', abbr: 'm', defaultsTo: 'Hello!');
   var results = parser.parse(arguments);
 
-  IpxMatrixBridge  bridge = new ipxbot.IpxMatrixBridge();
+  var  bridge = ipxbot.IpxMatrixBridge();
   //IpxBot bot = new IpxBot(results);
   print('Start of prog!');
   doWork(bridge, results);
@@ -24,27 +23,27 @@ void main(List<String> arguments)
 }
  void doWork(IpxMatrixBridge  bridge, ArgResults results )
  async {
-   String roomid =(results["roomid"].isNotEmpty)?results["roomid"]:"";
+   String roomid =(results['roomid'].isNotEmpty)?results['roomid']:'';
    print("connecting to server ${results["server"]}");
-   await bridge.connect(results["server"]);
+   await bridge.connect(results['server']);
    print("logging in with  ${results["user"]} and ${results["passwd"]}");
-   await bridge.login(user: results["user"], passwd: results["passwd"]);
+   await bridge.login(user: results['user'], passwd: results['passwd']);
    if(roomid.isEmpty) {
      print(
-         "trying to crearte room= ${results["room"]} and inviting ${results["invite"]}");
-     roomid = await bridge.createRoom(results["room"], invites: results["invite"]);
+         "trying to create room= ${results["room"]} and inviting ${results["invite"]}");
+     roomid = await bridge.createRoom(results['room'], invites: results['invite']);
    }
 
    if(await bridge.joinRoom(roomid))
      {
        //print("login = ${log} ");
        print("trying to send  ${results["msg"]}!");
-       await bridge.sendMsg(results["msg"]);
+       await bridge.sendMsg(results['msg']);
      }
  }
 
  String capas(IpxMatrixBridge  bridge)
  {
-   String data = bridge.capabilitites();
+   var data = bridge.capabilitites();
    return data;
  }
--- test/ipxbot_test.dart
+++ test/ipxbot_test.dart
@@ -2,7 +2,34 @@ import 'package:ipxbot/ipxbot.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
+    var results = {
+    'server': 'https://matrix.org',
+      'user':'chaletbot',
+      'passwd' :'ur3jHtaCiSUQbfS',
+    'room': '#bboettsroom:matrix.org',
+    'invite':'@bboett:matrix.org',
+    'roomid': '!OXOtvaYQZByMfTUVsg:matrix.org'
+  };
+    var  bridge = IpxMatrixBridge();
+  test('calculate', ()
+  {
+    String roomid =(results['roomid'].isNotEmpty)?results['roomid']:'';
+    print("connecting to server ${results["server"]}");
+    bridge.connect(results['server']);
     expect(calculate(), 42);
+    print("logging in with  ${results["user"]} and ${results["passwd"]}");
+    bridge.login(user: results['user'], passwd: results['passwd']);
+    if(roomid.isEmpty) {
+      print(
+          "trying to create room= ${results["room"]} and inviting ${results["invite"]}");
+      roomid = bridge.createRoom(results['room'], invites: results['invite']);
+    }
+
+    if(bridge.joinRoom(roomid))
+    {
+    //print("login = ${log} ");
+    print("trying to send  ${results["msg"]}!");
+    bridge.sendMsg(results['msg']);
+    }
   });
 }
